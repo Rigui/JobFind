503cfe10bd6f46613097c7634c1f83ed2407dcc3
rigui <alejandro.rigueira@gmail.com>
2016-06-04 17:58:36 +0200

    Refactor file name of infojobs.py

--- Prueba.py
+++ Prueba.py
@@ -1,8 +1,9 @@
 import scrapping_uvigo
-import rest
+import infojobs
 import mongodb
 
 print "Prueba.py\n"
+
 print "Obtener base de datos"
 ofertas_db = mongodb.get_db()
 
@@ -13,7 +14,7 @@ for oferta_uvigo in ofertas_uvigo:
     print oferta_uvigo
 
 print "\nObtener ofertas de Infojobs"
-ofertas_infojobs = rest.get_infojobs()
+ofertas_infojobs = infojobs.get_infojobs()
 for each_oferta in ofertas_infojobs:
     mongodb.add_oferta(ofertas_db, each_oferta)
     print each_oferta
