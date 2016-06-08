211bff3952dd064e3b13515c822d21b41bdcfabe
snsyn <alexmartinmerino@gmail.com>
2016-06-08 16:56:11 +0200

    finalizado enriq

--- enriquecimiento.py
+++ enriquecimiento.py
@@ -7,13 +7,18 @@ def enriquecimiento():
     database_ip = "52.208.8.144"
     database_port = 8080
 
-    colOfer = mongodb.conectar(database_ip, database_port, "ofertas")
-    colReq = mongodb.conectar(database_ip, database_port, "requisitos")
-    colTit = mongodb.conectar(database_ip, database_port, "titulos")
+    colOfer = mongodb.get_db(database_ip, database_port, "ofertas")
+    colTit = mongodb.get_db(database_ip, database_port, "titulos")
+    # rellena la experiencia de aquellas que no lo tengan (no actualiza -> el objetivo es completar, no inventar)
     docs = colOfer.find({'experiencia_min': {'$type': 10}})
 
+    cont = 0
     print("inicio exp")
     for doc in docs:
         Gen.generarExperiencia(colOfer, doc)
+        cont += 1
+        print cont
     print("inicio relaciones")
     Emp.relacionadas(colOfer, colTit)
+
+enriquecimiento()
